55da1279c3b24e7877333febacb04258456c28db
ns102894 <nserrano@harriscomputer.com>
2025-09-25 20:00:46 -0600

    Solved 252 in java

--- Java/MeetingRooms252.java
+++ Java/MeetingRooms252.java
@@ -9,7 +9,6 @@ public class MeetingRooms252 {
         public boolean canAttendMeetings(int[][] intervals) {
         Arrays.sort(intervals, Comparator.comparingInt(a -> a[0]));
         for(int i=0; i < intervals.length -1; i++){
-
             if(intervals[i][1] > intervals[i + 1][0]){
                 return false;
             }
