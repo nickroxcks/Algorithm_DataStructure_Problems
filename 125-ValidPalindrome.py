b64b05f0a2cddcfa6c4090ce67a7e6cfea86a2d3
ns102894 <nserrano@harriscomputer.com>
2025-12-24 14:58:25 -0700

    Solved problem 125 again in python

--- 125-ValidPalindrome.py
+++ 125-ValidPalindrome.py
@@ -11,28 +11,20 @@ O(1) memory
 
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-
         p1 = 0
         p2 = len(s) - 1
 
-        while (p1 < p2):
+        while p1 < p2:
             left = s[p1]
             right = s[p2]
 
-            if (left.isalnum() and right.isalnum()):
-                if (left.lower() == right.lower()):
-                    p1+=1
-                    p2-=1
-                    continue
-                else:
-                    return False
-
-            elif (not left.isalnum() and right.isalnum()):
+            if not left.isalnum():
                 p1 +=1
-            elif (left.isalnum() and not right.isalnum()):
+            elif not right.isalnum():
                 p2 -=1
+            elif left.lower() != right.lower():
+                return False
             else:
-                p1+=1
+                p1 +=1
                 p2 -=1
-
         return True
