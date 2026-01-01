d846a5a82262ed2d8b055a07207803d874c5a327
ns102894 <nserrano@harriscomputer.com>
2026-01-01 15:59:26 -0700

    Re-solved problem 206 in python

--- 206-ReversedLinkedList.py
+++ 206-ReversedLinkedList.py
@@ -5,15 +5,20 @@ class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
+'''
+My Solution:
 
+Reverse list in single pass via remembering the previous node
+
+O(n) time
+O(1) memory
+'''
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        prevNode = None
-        curNode = head
-        while(curNode):
-            nextNode = curNode.next
-            curNode.next = prevNode
-            prevNode = curNode
-            curNode = nextNode
-
-        return prevNode
+        prev = None
+        while head:
+            next = head.next
+            head.next = prev
+            prev = head
+            head = next
+        return prev
