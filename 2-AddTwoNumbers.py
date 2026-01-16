f48f12b96a9330ad9fefcf7db7c977d473a5e1e8
ns102894 <nserrano@harriscomputer.com>
2026-01-15 18:29:28 -0700

    Solved problem 2 in python

--- 2-AddTwoNumbers.py
+++ 2-AddTwoNumbers.py
@@ -6,39 +6,41 @@ class ListNode:
 from typing import Optional
 
 
+'''
+My Solution:
+
+Linear scan while adding sums and storing carry values each step of the way
+
+O(n) time
+O(1) memory
+
+'''
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
 
-        # guaranteed each list has a size of minimium 1
-        head = ListNode()
-        cur_node = head
         p1 = l1
         p2 = l2
         carry = 0
-        while (p1 or p2):
+        solPrev = ListNode(0)
+        prevNode = solPrev
 
-            val1 = p1.val if p1 else 0
+        while p1 or p2 or carry:
+            newNode = ListNode()
+
+            val1 =  p1.val if p1 else 0
             val2 = p2.val if p2 else 0
-            sum_val = val1 + val2 + cur_node.val
-            carry = 1 if sum_val >= 10 else 0
-            sum_val = sum_val % 10
+            curr_sum = val1 + val2 + carry
 
-            cur_node.val = sum_val
-
-            if (p1 and p1.next):
-                p1 = p1.next
+            if curr_sum >=10:
+                curr_sum = curr_sum % 10
+                carry = 1
             else:
-                p1 = None
-            if (p2 and p2.next):
-                p2 = p2.next
-            else:
-                p2 = None
-            # p1 = p1.next if p1.next else None
-            # p2 = p2.next if p2.next else None
+                carry = 0
 
-            if (p1 or p2 or carry):
-                new_node = ListNode(carry)
-                cur_node.next = new_node
-                cur_node = new_node
+            newNode.val = curr_sum
+            p1 = p1.next if p1 else None
+            p2 = p2.next if p2 else None
+            prevNode.next = newNode
+            prevNode = newNode
 
-        return head
+        return solPrev.next
