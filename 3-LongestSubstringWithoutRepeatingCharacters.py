52431929bb9b1c688549cb1b2c3f9d0998967362
ns102894 <nserrano@harriscomputer.com>
2026-01-22 19:15:31 -0700

    Solved problem 3 in python again

--- 3-LongestSubstringWithoutRepeatingCharacters.py
+++ 3-LongestSubstringWithoutRepeatingCharacters.py
@@ -1,3 +1,20 @@
+'''
+My Solution:
+
+Sliding Window
+
+We have a left and right pointer which set the boundary of the current window.
+The window is the current valid substring with no repeating characters.
+
+The right pointer moves up every iteration, and we use a set to remember if we saw the character already.
+
+If at any point we find the window is not a "substring with no repeating characters", we keep moving
+the left pointer and removing from our set up until the window condition is satisfied.
+
+O(n) time
+O(n) memory
+'''
+
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         sol = 0
@@ -12,7 +29,3 @@ class Solution:
         return sol
 
 
-
-test = Solution()
-
-print(test.lengthOfLongestSubstring('abcdefghijklmnopqrstuvwxyz'))
